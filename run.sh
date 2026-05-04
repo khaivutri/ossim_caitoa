10a84cbe5c17efe3e6daf06ca15df0ddeacc4722
KHANH TK <khanhnguyen07062006@gmail.com>
2026-05-04 13:23:47 +0700

    update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
-./os os_0_mlq_paging  > input/os_0_mlq_paging.output
-./os os_1_mlq_paging > input/os_1_mlq_paging.output
-./os os_1_mlq_paging_small_1K > input/os_1_mlq_paging_small_1K.output
-./os os_1_mlq_paging_small_4K > input/os_1_mlq_paging_small_4K.output
-./os os_1_singleCPU_mlq > input/os_1_singleCPU_mlq.output
-./os os_1_singleCPU_mlq_paging > input/os_1_singleCPU_mlq_paging.output
-./os sched > input/sched.output
-./os sched_0 input/sched_0.output
-./os sched_1 > input/sched_1.output
-./os os_sc > input/os_sc.output
-./os os_syscall > input/os_syscall.output
-./os os_syscall_list > input/os_syscall_list.output
-mv input/*.output output
+./os os_0_mlq_paging  > logs/os_0_mlq_paging.output
+./os os_1_mlq_paging > logs/os_1_mlq_paging.output
+./os os_1_mlq_paging_small_1K > logs/os_1_mlq_paging_small_1K.output
+./os os_1_mlq_paging_small_4K > logs/os_1_mlq_paging_small_4K.output
+./os os_1_singleCPU_mlq > logs/os_1_singleCPU_mlq.output
+./os os_1_singleCPU_mlq_paging > logs/os_1_singleCPU_mlq_paging.output
+./os sched_0 > logs/sched_0.output
+./os sched_1 > logs/sched_1.output
+./os sched > logs/sched.output
+./os os_sc > logs/os_sc.output
+./os os_syscall > logs/os_syscall.output
+./os os_syscall_list > logs/os_syscall_list.output
+# mv logs/*.output logs
